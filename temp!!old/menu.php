85765645124e275567087245e8eb457ba1a3d35b
John <marcelih55@gmail.com>
2019-10-19 19:54:54 +0200

    Adding Main files from Coca-Orange-Vanila

---
+++ temp!!old/menu.php
@@ -0,0 +1,16 @@
+<?php
+
+afficheMenu($_GET["menu"]);
+
+function afficheMenu(string $typeMenu){
+    string $buttons[];
+    if ($typeMenu == "scupltures"){
+        $buttons[0] = 
+    }
+
+    for ($i=0; $i < ; $i++) { 
+        # code...
+    }
+}
+
+?>
